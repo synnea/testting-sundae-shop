c5092466d3db0ed52207ee47942915d2af0e5d8e
Carina Poell <carina.poell@nea-x.net>
2021-04-13 12:57:23 +0200

    finish MSW section

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
-  render(<Options optionType="scoops" />);
+  render(<Options optionType='scoops' />);
 
   // find images
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
@@ -13,4 +13,20 @@ test("displays image for each scoop option from server", async () => {
   // @ts-ignore
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType='toppings' />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
