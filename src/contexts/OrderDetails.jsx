7246643d457069d07413e8e2ddbf6bec25fa8f36
Carina Poell <carina.poell@nea-x.net>
2021-04-14 09:03:44 +0200

    add totalUpdates tests and code

--- src/contexts/OrderDetails.jsx
+++ src/contexts/OrderDetails.jsx
@@ -1,14 +1,7 @@
 import { createContext, useContext, useState, useMemo, useEffect } from "react";
 import { pricePerItem } from "../constants/index";
 
-// format number as currency
-function formatCurrency(amount) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(amount);
-}
+import { formatCurrency } from "../utilities/index";
 
 const OrderDetails = createContext();
 
